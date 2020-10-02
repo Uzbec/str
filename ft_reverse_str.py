15309503f0b470a6fc112e1b6061e6a996c85717
mnklop <44546223+mnklop@users.noreply.github.com>
2020-10-02 11:03:43 +0300

    Update ft_reverse_str.py

--- ft_reverse_str.py
+++ ft_reverse_str.py
@@ -3,6 +3,6 @@ import ft_len as l
 
 def ft_reverse_str(str):
     otv = ""
-    for i in range(-1, -l.ft_len(str), -1):
+    for i in range(-1, -l.ft_len(str) - 1, -1):
         otv += str[i]
     return otv
